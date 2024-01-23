a39590f9232821cb9cba45d6c76d638eec72c615
unknown <centinaraphael0157@gmail.com>
2024-01-24 00:56:25 +0800

    I found a flaw in the code, will try to see other solutions

--- find_income_tax_with_set_variables.py
+++ find_income_tax_with_set_variables.py
@@ -2,6 +2,11 @@
 #1st 1000 is 0% interest, 2nd 1000 is 10% interest, remainging is 20%
 print('This is to know your income tax in thousand ex. 3000')
 user_income = float(input('How much is your income? '))
-
-income_tax = ((int(user_income) - 2000)*2) + 1000
-print(income_tax)
+computation_variable_need = (user_income-2000)
+if user_income >= 1000:
+    income_tax = ((int(user_income) - 2000)*2) + 1000
+    print('For an income of $',user_income, 'the income tax will be')
+    print(income_tax)
+if user_income < 1000:
+    print("the inputted amount don't quialify the tax rules")
+    print('For an amount of ', user_income, 'The tax will be $ 0')
