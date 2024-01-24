32c90debaeaa0883c883da16393a1946be588e62
unknown <centinaraphael0157@gmail.com>
2024-01-24 20:04:04 +0800

    Trying print function

--- find_income_tax_with_set_variables.py
+++ find_income_tax_with_set_variables.py
@@ -3,11 +3,14 @@
 print('This is to know your income tax in thousand ex. 3000')
 user_income = float(input('How much is your income? '))
 
+print('Your income is', user_income, 'and your payable tax is')
+
 if user_income < 1000:
     income_tax = 0
+    print ('('+str(user_income), '* 0%) + (0 * 10%) + (0 * 2%) = $'+str(income_tax))
+
 if user_income in range (1000, 2000):
     income_tax = (user_income-1000) * 1
 if user_income > 2000:
     income_tax = [(user_income-2000)*2]+1000
 
-print(income_tax)
