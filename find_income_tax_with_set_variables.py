eeaaeef865827cf1422c2588a7367b26c3a6f733
unknown <centinaraphael0157@gmail.com>
2024-01-24 19:57:53 +0800

    found an unnecessary code

--- find_income_tax_with_set_variables.py
+++ find_income_tax_with_set_variables.py
@@ -1,8 +1,7 @@
 #Calculate income tax for the given income by adhering to the rules below
 #1st 1000 is 0% interest, 2nd 1000 is 10% interest, remainging is 20%
 print('This is to know your income tax in thousand ex. 3000')
-user_income = input('How much is your income? ')
-computation_variable_need = (user_income-2000)
+user_income = float(input('How much is your income? '))
 
 if user_income < 1000:
     income_tax = 0
