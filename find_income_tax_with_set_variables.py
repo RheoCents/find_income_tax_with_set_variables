1121f0b33cf50f2f7a6e754819df29442ef2fdcf
unknown <centinaraphael0157@gmail.com>
2024-01-24 19:56:14 +0800

    trying simpler coding

--- find_income_tax_with_set_variables.py
+++ find_income_tax_with_set_variables.py
@@ -1,20 +1,14 @@
 #Calculate income tax for the given income by adhering to the rules below
 #1st 1000 is 0% interest, 2nd 1000 is 10% interest, remainging is 20%
 print('This is to know your income tax in thousand ex. 3000')
-user_income = float(input('How much is your income? '))
+user_income = input('How much is your income? ')
 computation_variable_need = (user_income-2000)
 
-def zero_percent_rateb():
-    zero_percent_rate = user_income - 1000 
+if user_income < 1000:
+    income_tax = 0
+if user_income in range (1000, 2000):
+    income_tax = (user_income-1000) * 1
+if user_income > 2000:
+    income_tax = [(user_income-2000)*2]+1000
 
-for i in range (3):
-     if i == 1:
-         zero_percent_rate = (user_income-1000)
-     if i == 2:
-         one_percent_rate = (zero_percent_rate-1000)
-     if i == 3:
-         two_percent_rate  = (user_income-2000)
-         if two_percent_rate < 0:
-             two_percent_rate = 0
-income_tax = zero_percent_rate + one_percent_rate + two_percent_rate
 print(income_tax)
