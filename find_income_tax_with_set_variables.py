ec1110c8f6220da7b239b1130fa42f20e6e07d37
unknown <centinaraphael0157@gmail.com>
2024-01-24 20:21:24 +0800

    Print optimized + flawed income 2000 fixed

--- find_income_tax_with_set_variables.py
+++ find_income_tax_with_set_variables.py
@@ -1,7 +1,8 @@
 #Calculate income tax for the given income by adhering to the rules below
 #1st 1000 is 0% interest, 2nd 1000 is 10% interest, remainging is 20%
-print('This is to know your income tax in thousand ex. 3000')
-user_income = float(input('How much is your income? '))
+print('This is a calculator for your income ta based on these parameters')
+print('1st $1000.0 = 0% tax\n2nd $1000.0 = 1% tax\nonwards = 2% tax')
+user_income = float(input('\nSo how much is your income? '))
 
 print('Your income is', user_income, 'and your payable tax is')
 
@@ -9,7 +10,7 @@ if user_income < 1000:
     income_tax = 0
     print('($'+str(user_income), '* 0%) + ($0.0 * 10%) + ($0.0 * 2%) = $'+str(income_tax))
 
-if user_income in range (1000, 2000):
+if user_income in range (1000, 2001):
     income_tax = (user_income-1000) * 1
     print('($1000.0 * 0%) + ($'+str(income_tax),'* 10%) + ($0.0 * 2%) = $'+str(income_tax))
 
