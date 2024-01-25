856a4b502049c6954296f79465e4716a92981d22
unknown <centinaraphael0157@gmail.com>
2024-01-25 15:33:55 +0800

    Did not noticed I inputed wrong parameters by a single point or zero (Now Fixed)

--- find_income_tax_with_set_variables.py
+++ find_income_tax_with_set_variables.py
@@ -1,19 +1,19 @@
 #Calculate income tax for the given income by adhering to the rules below
 #1st 1000 is 0% interest, 2nd 1000 is 10% interest, remainging is 20%
 print('This is a calculator for your income ta based on these parameters')
-print('1st $1000.0 = 0% tax\n2nd $1000.0 = 1% tax\nonwards = 2% tax')
+print('1st $10,000.0 = 0% tax\n2nd $10,000.0 = 1% tax\nonwards = 2% tax')
 user_income = float(input('\nSo how much is your income? '))
 
-print('Your income is', user_income, 'and your payable tax is')
+print('Your income is $'+ str(user_income), 'and your payable tax is')
 
-if user_income < 1000:
+if user_income < 10000:
     income_tax = 0
     print('($'+str(user_income), '* 0%) + ($0.0 * 10%) + ($0.0 * 2%) = $'+str(income_tax))
 
-if user_income in range (1000, 2001):
-    income_tax = (user_income-1000) * 1
-    print('($1000.0 * 0%) + ($'+str(income_tax),'* 10%) + ($0.0 * 2%) = $'+str(income_tax))
+if user_income in range (10000, 20001):
+    income_tax = (user_income-10000) * 0.1
+    print('($10000.0 * 0%) + ($'+str(income_tax),'* 10%) + ($0.0 * 2%) = $'+str(income_tax))
 
-if user_income > 2000:
-    income_tax = ((user_income-2000)*2)+1000
-    print('($1000.0 * 0%) + ($1000.0* 10%) + ($'+str(user_income-2000), '* 2%) = $'+str(income_tax))
+if user_income > 20000:
+    income_tax = ((user_income-20000)*0.2)+(10000*0.1)
+    print('($10000.0 * 0%) + ($10000.0* 10%) + ($'+str(user_income-20000), '* 2%) = $'+str(income_tax))
