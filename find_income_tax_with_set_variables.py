f19fc1d24002942a6a874e621b172909280358a9
unknown <centinaraphael0157@gmail.com>
2024-01-25 15:37:11 +0800

    Print function optimized

--- find_income_tax_with_set_variables.py
+++ find_income_tax_with_set_variables.py
@@ -4,16 +4,16 @@ print('This is a calculator for your income ta based on these parameters')
 print('1st $10,000.0 = 0% tax\n2nd $10,000.0 = 1% tax\nonwards = 2% tax')
 user_income = float(input('\nSo how much is your income? '))
 
-print('Your income is $'+ str(user_income), 'and your payable tax is')
+print('\nYour income is $'+ str(user_income), 'and your payable tax is')
 
 if user_income < 10000:
     income_tax = 0
-    print('($'+str(user_income), '* 0%) + ($0.0 * 10%) + ($0.0 * 2%) = $'+str(income_tax))
+    print('($'+str(user_income), '* 0%) + ($0.0 * 10%) + ($0.0 * 20%) = $'+str(income_tax))
 
 if user_income in range (10000, 20001):
     income_tax = (user_income-10000) * 0.1
-    print('($10000.0 * 0%) + ($'+str(income_tax),'* 10%) + ($0.0 * 2%) = $'+str(income_tax))
+    print('($10000.0 * 0%) + ($'+str(income_tax),'* 10%) + ($0.0 * 20%) = $'+str(income_tax))
 
 if user_income > 20000:
     income_tax = ((user_income-20000)*0.2)+(10000*0.1)
-    print('($10000.0 * 0%) + ($10000.0* 10%) + ($'+str(user_income-20000), '* 2%) = $'+str(income_tax))
+    print('($10000.0 * 0%) + ($10000.0* 10%) + ($'+str(user_income-20000), '* 20%) = $'+str(income_tax))
